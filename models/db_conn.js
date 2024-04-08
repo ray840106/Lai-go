682241371437f3569a796f1ebcf5cb6e05f827aa
ray840106 <34862806+ray840106@users.noreply.github.com>
2024-04-08 16:32:09 +0800

    Update db_conn.js

--- models/db_conn.js
+++ models/db_conn.js
@@ -2,11 +2,6 @@ require('dotenv').config()
 let mysql = require('mysql2/promise');
 const {CHAT_HOST,CHAT_USER,CHAT_PASSWORD,CHAT_DATABASE}=process.env;
 
-// const CHAT_HOST = 'social.mysql.database.azure.com'
-// const CHAT_USER = 'social'
-// const CHAT_PASSWORD = 'Xup654bjo4'
-// const CHAT_DATABASE = 'chat'
-
 var conn = mysql.createPool({
     host: CHAT_HOST,
     user: CHAT_USER,
